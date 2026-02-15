1ec18db06569f55631450bee7c19cd1ed21208f5
parjanay <parudadwhal@gmail.com>
2026-02-15 14:13:26 -0600

    JWT intro

--- express/auth.js
+++ express/auth.js
@@ -1,9 +1,9 @@
 import express from "express";
 const app = express();
-const jwt = require("jsonwebtoken");
+import jwt from "jsonwebtoken";
 const jwtPassword = "123456";
 const port = 3008;
-import { z } from "zod";
+app.use(express.json());
 
 const ALL_USERS = [
   {
@@ -23,15 +23,59 @@ const ALL_USERS = [
   },
 ];
 
-const schema = z.array(z.number());
-app.use(express.json());
+function userExists(username, password) {
+  let userExists = false;
+  //   for (let u of ALL_USERS) {
+  //     console.log(u);
+  //     if (u.username == username && u.password == password) {
+  //       userExists = true;
+  //     }
+  //   }
 
-app.post("/", (req, res) => {
-  const kidneys = req.body.kidneys;
-  const response = schema.safeParse(kidneys);
-  console.log(response.success);
-  res.send({ response });
+  ALL_USERS.find((val, indx, arr) => {
+    if (val.username == username && val.password == password) {
+      userExists = true;
+    }
+  });
+
+  return userExists;
+}
+
+app.post("/signin", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!userExists(username, password)) {
+    return res.status(403).json({
+      msg: "User doesn't exist",
+    });
+  }
+
+  var token = jwt.sign({ username: username }, jwtPassword);
+  return res.json({
+    token,
+    username,
+  });
 });
+
+app.get("/users", (req, res) => {
+  const token = req.headers.authorization;
+  let remainingUsers;
+  try {
+    let decoded = jwt.verify(token, jwtPassword);
+    let username = decoded.username;
+    remainingUsers = ALL_USERS.filter((u) => u.username != username);
+  } catch (err) {
+    return res.status(403).json({
+      msg: "Invalid token",
+    });
+  }
+
+  res.json({
+    remainingUsers,
+  });
+});
+
 app.listen(port, () => {
   console.log("server started");
 });
