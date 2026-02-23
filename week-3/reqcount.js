a9b61074543e241e9d20ad364f078e8b40ffa442
parjanay <parudadwhal@gmail.com>
2026-02-23 17:39:40 -0600

    solved some question for middlewares

---
+++ week-3/reqcount.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+let requestCount = 0;
+
+function reqCounter(req, res, next) {
+  requestCount++;
+  console.log(requestCount);
+  next();
+}
+
+app.use(reqCounter);
+
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john" });
+});
+
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
+});
+
+app.get("/requestCount", function (req, res) {
+  res.status(200).json({ requestCount });
+});
+
+app.listen(3000, () => {
+  console.log("server is running");
+});
